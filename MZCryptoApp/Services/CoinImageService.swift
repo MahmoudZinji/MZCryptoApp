1dccf87af841ed0ccf7c93c13598b36a068bfbb5
Mahmoud <mahmoud.zinji@gmail.com>
2022-10-17 16:11:30 +0200

    Modifying the code to use images saved in file manager

--- MZCryptoApp/Services/CoinImageService.swift
+++ MZCryptoApp/Services/CoinImageService.swift
@@ -14,13 +14,27 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let foldername = "coin_images"
+    private let imageName: String
 
     init(coin: CoinModel) {
         self.coin = coin
-        getCoinsImage()
+        self.imageName = coin.id
+        getCoinImage()
     }
 
-    private func getCoinsImage() {
+    private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: foldername) {
+            self.image = savedImage
+            print("Retrieved Image from File Manager")
+        } else {
+            downloadCoinsImage()
+            print("Downloading Image Now")
+        }
+    }
+
+    private func downloadCoinsImage() {
         guard let url = URL(string: coin.image) else { return }
 
         imageSubscription = NetworkingManager.download(url: url)
@@ -28,8 +42,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.foldername)
             })
     }
 }
