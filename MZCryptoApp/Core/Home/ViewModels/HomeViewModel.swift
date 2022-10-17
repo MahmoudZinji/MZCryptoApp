df895499b22de667fa5312e7f9471fab0ae13d0b
Mahmoud <mahmoud.zinji@gmail.com>
2022-10-17 15:02:40 +0200

    Adding Data Service Class

--- MZCryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ MZCryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -6,16 +6,28 @@
 //
 
 import Foundation
+import Combine
 
 class HomeViewModel: ObservableObject {
 
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    var cancellables = Set<AnyCancellable>()
+
+    private let dataService = CoinDataService()
 
     init() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             self.allCoins.append(DeveloperPreview.instance.coin)
             self.portfolioCoins.append(DeveloperPreview.instance.coin)
         }
+    }
+
+    func addSubscribers() {
+        dataService.$allCoins
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
     }
 }
